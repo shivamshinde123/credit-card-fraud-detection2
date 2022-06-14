8a146d517bb8dd63152122b40401118ba6788179
Shivam Shinde <shivamshinde92722@gmail.com>
2022-06-14 23:03:37 +0530

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Credit-card-fraud-detection", 
+    author="Shivam Shinde", 
+    packages=find_packages(),
+    license="MIT"
+)

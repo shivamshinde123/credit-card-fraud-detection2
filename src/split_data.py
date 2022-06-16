e4ac0debf0a79a250861d4cff7e67e29b79a6305
Shivam Shinde <shivamshinde92722@gmail.com>
2022-06-16 23:48:08 +0530

    minor change

--- src/split_data.py
+++ src/split_data.py
@@ -3,8 +3,6 @@ from sklearn.model_selection import train_test_split
 from get_data import read_params
 import pandas as pd
 
-
-
 def split_data(config_path):
     
     config = read_params(config_path)
